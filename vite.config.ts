dd1f06d2b3b96a0086ab3f4f36d98670c7f086e4
Govind <govindkumargupta159@gmail.com>
2026-09-17 18:34:20 +0530

    Configure base repo path, 404 fallback, and gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Payroll_Attendence/',
   server: {
     host: true, // Exposes the dev server to your local network (Wi-Fi / LAN) for mobile devices
     port: 5173
